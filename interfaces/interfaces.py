b8a26a4d8ed698ee70619ffd739500ba3cd7c16a
Dellsperger, Severin <Severin.Dellsperger@ins.hsr.ch>
2020-03-13 00:56:04 +0100

    prototype inclusive database

--- interfaces/interfaces.py
+++ interfaces/interfaces.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from flask_restplus import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-import json
+from dataclasses import dataclass
 import netifaces 
 import os
 
@@ -10,65 +10,89 @@ app = Flask(__name__)
 api = Api(app=app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///interfaces.db'
 db = SQLAlchemy(app)
-db.create_all()
 
 # class hostname(db.Model):
 #    id = db.Column(db.Integer, primary_key=True)
 #    name = db.Column(db.String(50), unique=True, nullable=False)
 
+@dataclass
 class Interface(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id: int
+    logical_name: str
+    physical_name: str
+    delay: int
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     logical_name = db.Column(db.String(50), unique=True)
     physical_name = db.Column(db.String(50), unique=True)
-    delay = db.Column(db.Integer, nullable=False)
+    delay = db.Column(db.Integer)
+
+
+db.drop_all()
+db.create_all()
+db.session.commit()
+
+for name in netifaces.interfaces():
+    new_interface = Interface(physical_name=name)
+    print(name)
+    try:
+        db.session.add(new_interface)
+        db.session.commit()
+    except Exception as e:
+        print(f"Failed to create the interfaces entry for {name} in the DB - reason: {e}")
+        db.session.rollback()
+        db.session.close()
+
+@api.route('/api/v1/interfaces/')
+class InterfaceList(Resource):
+    def get(self):
+        return jsonify((Interface.query.all()))
+        """
+        returns a list of available interfaces and related information
+        """
+@api.route('/api/v1/interfaces/<string:name>'/)
+class Delay(Resource):
+    def get(self, name):
+        interface = Interface.query.filter_by(physical_name=name).first_or_404()
+        return jsonify(interface)
+        """
+        returns information about specific interface
+        """
+
+@api.route('/api/v1/interfaces/<string:name>/delay')
+class Delay(Resource):
+    def get(self, name):
+        interface = Interface.query.filter_by(physical_name=name).first_or_404()
+        return jsonify(interface.delay)
+        """
+        returns delay of specific interface
+        """
     
-    def __repr__(self):
-        return '<Interface %r>' % self.id
-
-# for name in netifaces.interfaces():
-#     new_interface = Interface(id=counter, physical_name=name)
-#     print(name)
-#     print(new_interface)
-#     counter += 1
-#     try:
-#         db.session.add(new_interface)
-#         db.session.commit()
-#     except:
-#         print("Failed to create the interfaces entry in the DB")
-
-# @api.route('/api/v1/interfaces/')
-# class InterfaceList(Resource):
-#     def get(self):
-#         #interfaces  = netifaces.interfaces()
-#         #return interfaces
-#         return "test"
-#         """
-#         returns a list of available interfaces
-#         """
-
-# @api.route('/api/v1/interfaces/<string:name>/delay')
-# class Interface(Resource):
-#     def get(self, name):
-#         command = f"tc qdisc show dev {name}"
-#         value = os.popen(command).read()
-#         return value
-#         """
-#         returns delay information about a specific interface
-#         """
-#     def put(self, name):
-#         delay = request.form['value']
-        
-
-#         interface = Interface.query.get_or_404(id)
-#         try:
-#             command = f"sudo tc qdisc add dev {name} root netem delay {delay}ms"
-#             db.session.add()
-#         os.system(command)
-#         return command
-#     def delete(self, name):
-#         command = f"sudo tc qdisc del dev {name} root netem delay 0"
-#         os.system(command)
-#         return "deleted"
+    def put(self, name):
+        interface = Interface.query.filter_by(physical_name=name).first_or_404()
+        delay = request.form['value']
+        try:
+            interface.delay = delay
+            db.session.add(interface)
+            db.session.commit()
+        except Exception as e:
+            return(f"Failed to update the delay for the interface {name} in the DB - reason: {e}")
+            db.session.rollback()
+        command = f"sudo tc qdisc add dev {name} root netem delay {delay}ms"
+        os.system(command)
+        return command
+    def delete(self, name):
+        interface = Interface.query.filter_by(physical_name=name).first_or_404()
+        try:
+            interface.delay = None
+            db.session.add(interface)
+            db.session.commit()
+        except Exception as e:
+            return(f"Failed to delete delay for the interface {name} in the DB - reason: {e}")
+            db.session.rollback()
+        command = f"sudo tc qdisc del dev {name} root netem delay 0"
+        os.system(command)
+        return command
  
         
       
