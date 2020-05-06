1ae718c1f92a91684e07d81ea14e6f9a85b756d5
Dellsperger, Severin <Severin.Dellsperger@ins.hsr.ch>
2020-05-06 13:37:22 +0200

    improved handling with env variables

--- project/config.py
+++ project/config.py
@@ -10,11 +10,12 @@ def configure_app(app):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
         app.config['SALT_MOCKUP'] = True
     else:
+        app.config['SALT_MOCKUP'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{user}:{pw}@{url}/{db}'.format(
             user=os.environ.get('POSTGRES_USER'),
             pw=os.environ.get('POSTGRES_PASSWORD'),
             url='{}:{}'.format(
                 os.environ.get('POSTGRES_HOST'),
                 os.environ.get('POSTGRES_PORT')),
-            db=os.environ.get('POSTGRES_DB')
+            db=os.environ.get('POSTGRES_DB')      
         )
