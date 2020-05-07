6775c9686fdd19f9e379578bd1d80dddd642e574
Ruben <ru_broe@gmx.ch>
2020-05-07 21:47:18 +0200

    adjusted Exception

--- project/exception.py
+++ project/exception.py
@@ -1,7 +1,7 @@
 class WemulateException(Exception):
 
     def __init__(self, code, message):
-        self.message = f"Statuscode: {str(code)} : {message}"
+        self.message = f"Error {str(code)}: {message}"
 
     def __str__(self):
         return self.message
