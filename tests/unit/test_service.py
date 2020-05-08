d17c351289bbbe88d1ad6d64300f05f0b642e7c7
Ruben <ru_broe@gmx.ch>
2020-05-08 21:01:17 +0200

    added further testcases

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -7,7 +7,7 @@ sys.path.insert(0, f'{path}/../../project')
 import mock
 import pytest
 from pytest_mock import mocker
-from mockup.object_mockup import ObjectMockup, ConnectionMockup
+from mockup.object_mockup import ConnectionMockup, DeviceMockup, InterfaceMockup
 from types import SimpleNamespace
 from unittest.mock import call
 
@@ -45,45 +45,44 @@ def setup_mocks(mocker):
     mocker.patch.object(utils, 'update_parameter')
     mocker.patch.object(utils, 'update_connection')
     mocker.patch.object(utils, 'delete_connection')
-    logical_interfaces = [SimpleNamespace(logical_interface_id=i) for i in range(1, 5)]
+    logical_interfaces = [SimpleNamespace(logical_interface_id=i,
+                                          logical_name='LAN-' + chr(ord('@') + i)) for i in range(1, 10)]
+    interfaces = [InterfaceMockup(interface_id=i + 1,
+                                  physical_name=f'eth{i}',
+                                  device_id=8,
+                                  has_logical_interface=logical_interfaces[i],
+                                  logical_name=logical_interfaces[i].logical_name
+                                  )
+                  for i in range(4)]
     connection = ConnectionMockup(
         connection_name='connection 1',
+        interface1=logical_interfaces[3].logical_name,
+        interface2=logical_interfaces[1].logical_name,
         first_logical_interface=logical_interfaces[3],
         second_logical_interface=logical_interfaces[1],
         active_device_profile=9
     )
-    utils.get_device.return_value = SimpleNamespace(device_id=8,
-                                                    device_name='test device name',
-                                                    active_profile=9,
-                                                    management_ip='192.168.0.1',
-                                                    interfaces=[
-                                                        SimpleNamespace(physical_name='eth1', device_id=8,
-                                                                        has_logical_interface=logical_interfaces[0]),
-                                                        SimpleNamespace(physical_name='eth2', device_id=8,
-                                                                        has_logical_interface=logical_interfaces[1]),
-                                                        SimpleNamespace(physical_name='eth3', device_id=8,
-                                                                        has_logical_interface=logical_interfaces[2]),
-                                                        SimpleNamespace(physical_name='eth4', device_id=8,
-                                                                        has_logical_interface=logical_interfaces[3])
-                                                    ],
-                                                    connections=[
-                                                        connection
-                                                    ])
+    profile = SimpleNamespace(profile_id=8,
+                              profile_name='profile 8 name',
+                              connections=[
+                                  connection
+                              ])
+    device = DeviceMockup(device_id=8,
+                          device_name='test device name',
+                          active_profile=9,
+                          active_profile_name=profile.profile_name,
+                          management_ip='192.168.0.1',
+                          interfaces=interfaces,
+                          connections=[
+                              connection
+                          ])
+    utils.get_device.return_value = device
     utils.is_device_present.return_value = False
     utils.get_device_list.return_value = [
-        SimpleNamespace(),
+        device
     ]
-    utils.get_active_profile.return_value = SimpleNamespace(profile_id=8,
-                                                            profile_name='profile 8 name',
-                                                            connections=[
-                                                                connection
-                                                            ])
-    utils.get_all_interfaces.return_value = [
-        SimpleNamespace(physical_name='eth1', device_id=8, logical_interface_id=1),
-        SimpleNamespace(physical_name='eth2', device_id=8, logical_interface_id=2),
-        SimpleNamespace(physical_name='eth3', device_id=8, logical_interface_id=3),
-        SimpleNamespace(physical_name='eth4', device_id=8, logical_interface_id=4)
-    ]
+    utils.get_active_profile.return_value = profile
+    utils.get_all_interfaces.return_value = interfaces
     utils.get_logical_interface.side_effect = logical_interfaces
     utils.get_logical_interface_by_name.side_effect = logical_interfaces
     utils.create_profile.return_value = SimpleNamespace(profile_id=9)
@@ -130,17 +129,16 @@ def test_create_duplicate_device(mocker):
         db.session.commit.assert_called_once()
         db.session.rollback.assert_not_called()
 
-# need to mock serialize for this to work
-# def test_get_device_list(mocker):
-#     device_list = service.get_device_list()
-#     assert device_list == [
-#         {
-#             'device_id': 8,
-#             'device_name': 'test device name',
-#             'management_ip': '192.168.0.1',
-#             'active_profile_name': 'profile 8 name'
-#         }
-#     ]
+def test_get_device_list(mocker):
+    device_list = service.get_device_list()
+    assert device_list == [
+        {
+            'device_id': 8,
+            'device_name': 'test device name',
+            'management_ip': '192.168.0.1',
+            'active_profile_name': 'profile 8 name'
+        }
+    ]
 
 def test_get_device_list_empty(mocker):
     utils.get_device_list.return_value = []
@@ -158,7 +156,32 @@ def test_get_device_inexistent(mocker):
         assert e.args[0] == 404
         assert e.args[1] == "Device with id 4 not found"
 
-# missing test for non empty, would need serlialize function
+def test_get_interface_list(mocker):
+    interface_list = service.get_interface_list(8)
+
+    assert interface_list == [
+        {
+            'interface_id': 1,
+            'logical_name': 'LAN-A',
+            'physical_name': 'eth0'
+        },
+        {
+            'interface_id': 2,
+            'logical_name': 'LAN-B',
+            'physical_name': 'eth1'
+        },
+        {
+            'interface_id': 3,
+            'logical_name': 'LAN-C',
+            'physical_name': 'eth2'
+        },
+        {
+            'interface_id': 4,
+            'logical_name': 'LAN-D',
+            'physical_name': 'eth3'
+        },
+    ]
+
 def test_get_interface_list_empty(mocker):
     utils.get_all_interfaces.return_value = []
 
@@ -166,7 +189,15 @@ def test_get_interface_list_empty(mocker):
 
     assert interface_list == []
 
-# missing test for non empty, would need serlialize function
+def test_get_connection_list(mocker):
+    connection_list = service.get_connection_list(8)
+
+    assert connection_list == [{
+        'connection_name': 'connection 1',
+        'interface1': 'LAN-D',
+        'interface2': 'LAN-B'
+    }]
+
 def test_get_connection_list_emtpy(mocker):
     utils.get_active_profile.return_value = SimpleNamespace(connections=[])
 
@@ -175,6 +206,31 @@ def test_get_connection_list_emtpy(mocker):
     assert connection_list == []
 
 def test_add_connection(mocker):
+    logical_interfaces = [SimpleNamespace(logical_interface_id=i, logical_name='LAN-' + chr(ord('@') + i)) for i in range(1, 10)]
+    interfaces = [InterfaceMockup(interface_id=i + 1,
+                                  physical_name=f'eth{i}',
+                                  device_id=8,
+                                  has_logical_interface=logical_interfaces[i],
+                                  logical_name=logical_interfaces[i].logical_name
+                                  )
+                  for i in range(4)]
+    profile = SimpleNamespace(profile_id=8,
+                              profile_name='profile 8 name',
+                              connections=[]
+                              )
+    device = DeviceMockup(device_id=8,
+                          device_name='test device name',
+                          active_profile=9,
+                          active_profile_name=profile.profile_name,
+                          management_ip='192.168.0.1',
+                          interfaces=interfaces,
+                          connections=[]
+                          )
+    utils.get_device.return_value = device
+    utils.get_active_profile.return_value = profile
+    utils.get_all_interfaces.return_value = interfaces
+    utils.get_logical_interface.side_effect = logical_interfaces
+    utils.get_logical_interface_by_name.side_effect = logical_interfaces
 
     service.update_connections(8, [
         {
@@ -189,18 +245,159 @@ def test_add_connection(mocker):
             'duplication': 50
         }
     ])
+    utils.create_connection.assert_called_with('connection 1', logical_interfaces[0], logical_interfaces[1], profile)
+    utils.create_parameter.assert_has_calls([call('bandwidth', 500, 1),
+                                            call('delay', 10, 1),
+                                            call('packet_loss', 30, 1),
+                                            call('jitter', 15, 1),
+                                            call('corruption', 40, 1),
+                                            call('duplication', 50, 1)],
+                                            any_order=True)
+    salt_api.add_connection.assert_called_with(device.device_name,
+                                               'connection 1',
+                                               interfaces[0].physical_name,
+                                               interfaces[1].physical_name)
+    salt_api.update_parameters.assert_called_with(device.device_name,
+                                               interfaces[0].physical_name,
+                                               {
+                                                   'bandwidth': 500,
+                                                   'delay': 10,
+                                                   'packet_loss': 30,
+                                                   'jitter': 15,
+                                                   'corruption': 40,
+                                                   'duplication': 50
+                                               })
+    db.session.commit.assert_called_once()
+    db.session.rollback.assert_not_called()
+
+
+
 
 def test_delete_connection(mocker):
-    pass
+    logical_interfaces = [SimpleNamespace(logical_interface_id=i,
+                                          logical_name='LAN-' + chr(ord('@') + i)) for i in range(1, 10)]
+    interfaces = [InterfaceMockup(interface_id=i + 1,
+                                  physical_name=f'eth{i}',
+                                  device_id=8,
+                                  has_logical_interface=logical_interfaces[i],
+                                  logical_name=logical_interfaces[i].logical_name
+                                  )
+                  for i in range(4)]
+    connection = ConnectionMockup(
+        connection_name='connection 1',
+        interface1=logical_interfaces[3].logical_name,
+        interface2=logical_interfaces[1].logical_name,
+        first_logical_interface=logical_interfaces[3],
+        second_logical_interface=logical_interfaces[1],
+        active_device_profile=9
+    )
+    profile = SimpleNamespace(profile_id=8,
+                              profile_name='profile 8 name',
+                              connections=[
+                                  connection
+                              ])
+    device = DeviceMockup(device_id=8,
+                          device_name='test device name',
+                          active_profile=9,
+                          active_profile_name=profile.profile_name,
+                          management_ip='192.168.0.1',
+                          interfaces=interfaces,
+                          connections=[
+                              connection
+                          ])
+    utils.get_device.return_value = device
+    utils.get_active_profile.return_value = profile
+    utils.get_all_interfaces.return_value = interfaces
+    utils.get_logical_interface.side_effect = logical_interfaces
+    utils.get_logical_interface_by_name.side_effect = logical_interfaces
+
+    service.update_connections(8, [])
+
+    utils.delete_connection.assert_called_with(connection)
+    salt_api.remove_connection.assert_called_with(device.device_name, connection.connection_name)
+    salt_api.remove_parameters.assert_called_with(device.device_name, interfaces[3].physical_name)
+    db.session.commit.assert_called_once()
+    db.session.rollback.assert_not_called()
 
 def test_update_connection(mocker):
-    pass
+    logical_interfaces = [SimpleNamespace(logical_interface_id=i,
+                                          logical_name='LAN-' + chr(ord('@') + i)) for i in range(1, 10)]
+    interfaces = [InterfaceMockup(interface_id=i + 1,
+                                  physical_name=f'eth{i}',
+                                  device_id=8,
+                                  has_logical_interface=logical_interfaces[i],
+                                  logical_name=logical_interfaces[i].logical_name
+                                  )
+                  for i in range(4)]
+    parameters = [
+        SimpleNamespace(parameter_name='bandwidth', value=500),
+        SimpleNamespace(parameter_name='delay', value=10),
+        SimpleNamespace(parameter_name='packet_loss', value=30),
+        SimpleNamespace(parameter_name='jitter', value=15),
+        SimpleNamespace(parameter_name='corruption', value=40),
+        SimpleNamespace(parameter_name='duplication', value=50)
+    ]
+    connection = ConnectionMockup(
+        connection_name='connection 1',
+        interface1=logical_interfaces[0].logical_name,
+        interface2=logical_interfaces[1].logical_name,
+        first_logical_interface=logical_interfaces[0],
+        second_logical_interface=logical_interfaces[1],
+        active_device_profile=9,
+        parameters=parameters
+    )
+    profile = SimpleNamespace(profile_id=8,
+                              profile_name='profile 8 name',
+                              connections=[
+                                  connection
+                              ])
+    device = DeviceMockup(device_id=8,
+                          device_name='test device name',
+                          active_profile=9,
+                          active_profile_name=profile.profile_name,
+                          management_ip='192.168.0.1',
+                          interfaces=interfaces,
+                          connections=[
+                              connection
+                          ])
+    utils.get_device.return_value = device
+    utils.get_active_profile.return_value = profile
+    utils.get_all_interfaces.return_value = interfaces
+    utils.get_logical_interface.side_effect = logical_interfaces
+    utils.get_logical_interface_by_name.side_effect = logical_interfaces
 
-def test_add_second_connection(mocker):
-    pass
+    service.update_connections(8, [
+        {
+            'connection_name': 'connection 1',
+            'interface1': 'LAN-A',
+            'interface2': 'LAN-B',
+            'bandwidth': 1000,
+            'delay': 11,
+            'packet_loss': 50,
+            'jitter': 20,
+            'corruption': 55,
+            'duplication': 10
+        }
+    ])
 
-def test_delete_second_connection(mocker):
-    pass
-
-def test_no_update_connection(mocker):
-    pass
+    utils.update_parameter.assert_has_calls([
+        call(parameters[0], 1000),
+        call(parameters[1], 11),
+        call(parameters[2], 50),
+        call(parameters[3], 20),
+        call(parameters[4], 55),
+        call(parameters[5], 10)],
+        any_order=True)
+    salt_api.update_parameters.assert_called_with(
+        device.device_name,
+        interfaces[0].physical_name,
+        {
+            'bandwidth': 1000,
+            'delay': 11,
+            'packet_loss': 50,
+            'jitter': 20,
+            'corruption': 55,
+            'duplication': 10
+        })
+    db.session.commit.assert_called_once()
+    db.session.rollback.assert_not_called()
