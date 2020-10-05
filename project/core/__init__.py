8a71fa5686fb0b8a265b5f6a22c29002ccca6f23
Severin Dellsperger <severin.dellsperger@gmail.com>
2020-10-05 12:52:49 +0200

    Initial logger implementation

--- project/core/__init__.py
+++ project/core/__init__.py
@@ -6,14 +6,26 @@ from core.database import db
 import core.database.utils as dbutils
 from config import configure_app
 from mockup.salt_mockup import SaltMockup
+import logging
 
 def create_app():
     app = Flask(__name__)
-
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    logger = app.logger
+    
+    logger.info("BEGIN configuring app")
     configure_app(app)
+    logger.info("FINISH configuring app")
+    logger.info("BEGIN INIT DB")
     db.init_app(app)
+    logger.info("FINISH INIT DB")
     app.app_context().push()
+    logger.info("CREATE DB TABLES")
     db.create_all()
+    logger.info("FINISH DB TABLES")
+
 
     # Create logical interfaces if not exist
     if not len(dbutils.get_logical_interface_list()):
