28cf6f72cdd52aead163cbd6df273b1d919e6f5c
Severin Dellsperger <severin.dellsperger@gmail.com>
2020-10-16 13:02:52 +0200

    add deletion of interface after restart and change distribution of jitter

--- project/config.py
+++ project/config.py
@@ -5,12 +5,13 @@ def configure_app(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     if os.environ.get('WEMULATE_TESTING') == 'True':
-        print('Wemulate Testing Configuration')
+        app.logger.debug('Using Wemulate Testing Configuration inclusive SQLLite DB and Mockup')
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
         app.config['SALT_MOCKUP'] = True
     else:
         app.config['SALT_MOCKUP'] = False
+        app.logger.debug('Using Wemulate Real Live Configuration inclusive Postgres DB and Mockup')
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{user}:{pw}@{url}/{db}'.format(
             user=os.environ.get('POSTGRES_USER'),
             pw=os.environ.get('POSTGRES_PASSWORD'),
--- project/core/__init__.py
+++ project/core/__init__.py
@@ -13,24 +13,29 @@ def create_app():
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    logger = app.logger
     
-    logger.info("BEGIN configuring app")
+    app.logger.info("BEGIN configuring app")
     configure_app(app)
-    logger.info("FINISH configuring app")
-    logger.info("BEGIN INIT DB")
+    app.logger.info("FINISH configuring app")
+    app.logger.info("BEGIN INIT DB")
     db.init_app(app)
-    logger.info("FINISH INIT DB")
+    app.logger.info("FINISH INIT DB")
     app.app_context().push()
-    logger.info("CREATE DB TABLES")
+    app.logger.info("CREATE DB TABLES")
     db.create_all()
-    logger.info("FINISH DB TABLES")
+    app.logger.info("FINISH DB TABLES")
 
 
     # Create logical interfaces if not exist
     if not len(dbutils.get_logical_interface_list()):
+        app.logger.info("Create logical Interfaces")
         dbutils.create_logical_interfaces()
         db.session.commit()
+    
+    # Delete Connection if exists after restart
+    if len(dbutils.get_connection_list()):
+        app.logger.info("Create logical Interfaces")
+        dbutils.delete_present_connection()
 
     CORS(app)
 
